b3ed4d5344f1d54c4621b3e2f7c9cf1862f46903
James Inman <james@jamesinman.co.uk>
2025-12-19 11:42:05 +0000

    Switching all imports to esm.sh - better ES module handling than jsdelivr

--- config/importmap.rb
+++ config/importmap.rb
@@ -15,12 +15,12 @@ pin "panda/editor/plain_text_editor", to: "/panda/editor/plain_text_editor.js"
 pin "panda/editor/css_extractor", to: "/panda/editor/css_extractor.js"
 pin "panda/editor/rich_text_editor", to: "/panda/editor/rich_text_editor.js"
 
-# EditorJS Core and plugins (from CDN)
-pin '@editorjs/editorjs', to: 'https://cdn.jsdelivr.net/npm/@editorjs/editorjs@2.28.2/+esm'
-pin '@editorjs/paragraph', to: 'https://cdn.jsdelivr.net/npm/@editorjs/paragraph@2.11.3/+esm'
-pin '@editorjs/header', to: 'https://cdn.jsdelivr.net/npm/@editorjs/header@2.8.1/+esm'
-pin '@editorjs/nested-list', to: 'https://cdn.jsdelivr.net/npm/@editorjs/nested-list@1.4.2/+esm'
-pin '@editorjs/quote', to: 'https://cdn.jsdelivr.net/npm/@editorjs/quote@2.6.0/+esm'
-pin '@editorjs/simple-image', to: 'https://cdn.jsdelivr.net/npm/@editorjs/simple-image@1.6.0/+esm'
-pin '@editorjs/table', to: 'https://cdn.jsdelivr.net/npm/@editorjs/table@2.3.0/+esm'
-pin '@editorjs/embed', to: 'https://cdn.jsdelivr.net/npm/@editorjs/embed@2.7.0/+esm'
+# EditorJS Core and plugins (from esm.sh - better ES module handling than jsdelivr)
+pin '@editorjs/editorjs', to: 'https://esm.sh/@editorjs/editorjs@2.28.2'
+pin '@editorjs/paragraph', to: 'https://esm.sh/@editorjs/paragraph@2.11.3'
+pin '@editorjs/header', to: 'https://esm.sh/@editorjs/header@2.8.1'
+pin '@editorjs/nested-list', to: 'https://esm.sh/@editorjs/nested-list@1.4.2'
+pin '@editorjs/quote', to: 'https://esm.sh/@editorjs/quote@2.6.0'
+pin '@editorjs/simple-image', to: 'https://esm.sh/@editorjs/simple-image@1.6.0'
+pin '@editorjs/table', to: 'https://esm.sh/@editorjs/table@2.3.0'
+pin '@editorjs/embed', to: 'https://esm.sh/@editorjs/embed@2.7.0'
